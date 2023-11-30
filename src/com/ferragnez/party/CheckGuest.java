b1010f3235cac4ce433e6268e15e1c90e5c04468
GiovanniLafortezza <recordeknot388@gmail.com>
2023-11-30 14:22:39 +0100

    Declared the array with the list of guests

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,10 +1,35 @@
 package com.ferragnez.party;
 
 
+import java.util.Scanner;
 
 public class CheckGuest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String[] listGuests = {
+                "Dua Lipa",
+                "Paris Hilton",
+                "Manuel Agnelli",
+                "J-AX",
+                "Francesco Totti",
+                "Ilary Blasi",
+                "Bebe Vio",
+                "Luis",
+                "Pardis Zarei",
+                "Martina Maccherone",
+                "Rachel Zeilic"
+        };
+
+        System.out.println("Inserire nome invitato");
+        String nameGuest = scanner.nextLine();
+
+        for (int i = 0; i < listGuests.length; i++) {
+            if(nameGuest.equals(listGuests[i])) {
+
+            }
+        }
+        scanner.close();
     }
 
 }
